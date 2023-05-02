128ad50f0376c0878cfae5d67315f7dc82dce7da
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-05-03 00:46:53 +0200

    Zadanie 2 - przycisk zmieniający kolor

---
+++ script.js
@@ -0,0 +1,161 @@
+const vertexShaderTxt = `
+    precision mediump float;
+
+    attribute vec2 vertPosition;
+    attribute vec3 vertColor;
+
+    varying vec3 fragColor;
+
+    void main()
+    {
+        fragColor = vertColor;
+        gl_Position = vec4(vertPosition, 0.0, 1.0);
+    }
+`
+
+const fragmentShaderTxt = `
+    precision mediump float;
+
+    varying vec3 fragColor;
+    void main(){
+        gl_FragColor = vec4(fragColor, 1.0);
+    }
+`
+const canvas = document.getElementById("main-canvas");
+    const gl = canvas.getContext("webgl");
+
+    if(!gl){
+        alert("no webgl");
+    }
+
+const Triangle = function () {
+    
+    gl.clearColor(0.5, 0.5, 0.9, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.log(gl.getShaderInfoLog(vertexShader));
+    }
+    gl.compileShader(fragmentShader);
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.log(gl.getShaderInfoLog(fragmentShader));
+    }
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    let triangleVert = [
+        //X, Y          RGB
+        0.0, 0.5,       1.0, 0.0, 0.0,
+        -0.5, -0.5,     0.0, 1.0, 0.0,
+        0.5, -0.5,      0.0, 0.0, 1.0
+    ]
+    const triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVert), gl.STATIC_DRAW);
+
+    const posAttrLocation = gl.getAttribLocation(program, 'vertPosition');
+    const colorAttrLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        posAttrLocation,
+        2,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0,
+    );
+
+    gl.vertexAttribPointer(
+        colorAttrLocation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+    );
+
+    gl.enableVertexAttribArray(posAttrLocation);
+    gl.enableVertexAttribArray(colorAttrLocation);
+
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
+
+}
+
+function changeColor() {
+    gl.clearColor(0.5, 0.5, 0.9, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderTxt);
+    gl.shaderSource(fragmentShader, fragmentShaderTxt);
+
+    gl.compileShader(vertexShader);
+    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+        console.log(gl.getShaderInfoLog(vertexShader));
+    }
+    gl.compileShader(fragmentShader);
+    if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+        console.log(gl.getShaderInfoLog(fragmentShader));
+    }
+
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.validateProgram(program);
+
+    const color = getRandomColor();
+
+    let triangleVert = [
+        //X, Y          RGB
+        0.0, 0.5,       color[0], color[1], color[2],
+        -0.5, -0.5,     color[1], color[2], color[0],
+        0.5, -0.5,      color[2], color[0], color[1]
+    ]
+    const triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVert), gl.STATIC_DRAW);
+
+    const colorAttrLocation = gl.getAttribLocation(program, 'vertColor');
+
+
+    gl.vertexAttribPointer(
+        colorAttrLocation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT,
+    );
+
+    gl.enableVertexAttribArray(colorAttrLocation);
+
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
+}
+function getRandomColor() {
+    return [Math.random(), Math.random(), Math.random()];
+}
